5efaed6dcdb6471023ab08154f7daf9988533264
armares2001 <armares2001@libero.it>
2023-10-12 22:23:10 +0200

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import data from '../../data.json';
 
+if (typeof data === 'object') data.myName = 'Piero';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit {
+  title: string;
+  name: string;
+
+  constructor() {
+    this.title = 'my-first-app';
+    this.name = data.myName;
+  }
+
+  ngOnInit(): void {}
+
+  success(): void {
+    this.name = this.name + '0';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { HostComponent } from './host/host.component';
+import { TestDirective } from './directives/test.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    HostComponent,
+    TestDirective,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
